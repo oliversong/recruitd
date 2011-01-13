caa04a1d5d96478c0ea7d9d416c782df32b1d888
Keone Hon <keone.hon@gmail.com>
2011-01-13 03:50:40 -0500

    remove extraneous controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -144,7 +144,12 @@ Recruitd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "students#index"
+  #root :to => ""
+  authenticate :user do
+    root :to => "c#home"
+  end
+  
+  root :to => "devise/sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
