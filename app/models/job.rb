cce61b79b703669a654e135868a3a25df987c37b
Keone Hon <keone.hon@gmail.com>
2011-01-16 01:51:24 -0500

    jobs in s/manage

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,7 @@ class Job < ActiveRecord::Base
   belongs_to :company
   has_many :career_jobs
   has_many :careers, :through => :career_jobs
+  has_many :student_labelings
   
   def name
     return title + " at " + company.name
